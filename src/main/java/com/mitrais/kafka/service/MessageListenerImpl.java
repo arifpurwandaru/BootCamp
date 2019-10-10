af25ba9668dde50911628a4d5e2e28877ceb491b
Arif Nazar Purwandaru <arifnazar.purwandaru@mitrais.com>
2019-10-10 09:28:28 +0700

    update message listener

--- src/main/java/com/mitrais/kafka/service/MessageListenerImpl.java
+++ src/main/java/com/mitrais/kafka/service/MessageListenerImpl.java
@@ -1,15 +1,25 @@
 package com.mitrais.kafka.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.mitrais.persistence.entity.MUser;
+import com.mitrais.persistence.service.UserDAO;
+import com.mitrais.utils.JsonUtil;
+
 @Service
 public class MessageListenerImpl implements MessageListener{
-
+	
+	@Autowired
+	private UserDAO dao;
+	
 	@Override
 	@KafkaListener(topics="${kafka.my.topic}")
 	public void listen(String jsonStr) throws Exception {
-		System.out.println("======================> Message: "+jsonStr);
+		MUser user = JsonUtil.parseJson(jsonStr, MUser.class);
+		dao.save(user);
+		System.out.println("======================> Consumed Message: "+jsonStr);
 	}
 
 }
